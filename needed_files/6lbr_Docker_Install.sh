8161af09a70873606f822c44d2293523e14cccda
Selleslagh Tom <Tom@MacBook-Pro-de-Selleslagh-2.local>
2018-09-26 11:27:17 +0200

    Fix the ipv6 address link

--- needed_files/6lbr_Docker_Install.sh
+++ needed_files/6lbr_Docker_Install.sh
@@ -23,14 +23,9 @@ make install
 make plugins-install
 update-rc.d 6lbr defaults
 echo "Deploying settings files"
-cd /
-netstat -r6 | awk '/eth0/{print $1}' | grep '/64' | grep -v 'fe80' |head -n 1|cut -d'/' -f1 > sixlbrnet
+mv /app/6lbr/examples/6lbr/tools/nvm_tool /nvm_tool
 cp /needed_files/6lbr.conf /etc/6lbr/6lbr.conf
 cp /needed_files/nvm.dat /etc/6lbr/nvm.dat
-cd /app/6lbr/examples/6lbr/tools
-./nvm_tool --update --wsn-prefix $(cat /sixlbrnet) /etc/6lbr/nvm.dat
-./nvm_tool --update --wsn-context-0 $(cat /sixlbrnet) /etc/6lbr/nvm.dat
-./nvm_tool --print /etc/6lbr/nvm.dat > decoded_nvm.txt
 echo "Cleaning not needed files"
 apt-get remove --auto-remove --purge -y git
 apt-get remove --auto-remove --purge -y build-essential
--- needed_files/6lbr_Docker_OnBoot.sh
+++ needed_files/6lbr_Docker_OnBoot.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 brctl addbr br0
+cd /
+netstat -r6 | awk '/eth0/{print $1}' | grep '/64' | grep -v 'fe80' |head -n 1|cut -d'/' -f1 > sixlbrnet
+./nvm_tool --update --wsn-prefix $(cat /sixlbrnet) /etc/6lbr/nvm.dat
+./nvm_tool --update --wsn-context-0 $(cat /sixlbrnet) /etc/6lbr/nvm.dat
 service 6lbr start
-sleep 10
+sleep 7
 brctl addif br0 eth0 tap0
 ip link set br0 up
 sleep 5
