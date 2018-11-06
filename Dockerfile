c7c505ef1fbd0f9444d14874202d375df713aa7f
Selleslagh Tom <selltom@MacBook-Pro-de-Selleslagh.local>
2018-11-06 14:31:25 +0100

    6LBR beta

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM debian
 ADD needed_files_6lbr ./needed_files_6lbr
 RUN chmod -R +x /needed_files_6lbr && needed_files_6lbr/6lbr_Docker_Install.sh
-ADD needed_files_tayga ./needed_files_tayga
-RUN chmod -R +x /needed_files_tayga && needed_files_tayga/Tayga_Docker_Install.sh
 ENV LWM2MSERVER="cccc::64:ac11:2"
 CMD needed_files_6lbr/6lbr_Docker_OnBoot.sh
--- needed_files_6lbr/6lbr_Docker_OnBoot.sh
+++ needed_files_6lbr/6lbr_Docker_OnBoot.sh
@@ -10,16 +10,7 @@ echo 1 > /proc/sys/net/ipv6/conf/eth0/forwarding
 echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 ip addr add bbbb::1/64 dev tap0
 ip addr add cccc::1/64 dev tap0
-tayga --mktun
-ip link set nat64 up
 echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-echo 0 > /proc/sys/net/ipv6/conf/nat64/disable_ipv6
-echo 1 > /proc/sys/net/ipv6/conf/nat64/forwarding
-ip addr add $(netinfo --ipv4router) dev nat64
-ip addr add cccc::2/64 dev nat64
-ip route add cccc::64:0:0/96 dev nat64
-ip route add 172.46.0.0/16 dev nat64
-tayga
 sleep 5
 cat /var/log/6lbr.ip
 tail -f /var/log/6lbr.log
